6bfb8be19536cb8836aefabeeda753e2c17b94d6
Krhorsch <krhorsch@yahoo.com>
2018-06-07 19:09:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'dc5effa1919461429938ed36286b7b452de0b5a5'
+fetch('https://api.github.com/Krhorsch/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+  }).then(res => res.json()).then(json => console.log(json));
